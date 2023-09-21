098683c5ade1d925891d3b233e50c61f7a954e39
Oluwagbemileke <akanjisamuel6@gmail.com>
2023-09-21 09:12:07 +0100

    questionPage sorted waiting for results

--- frontend/bible-quiz-webapp/src/pages/Question Page/QuestionPage.js
+++ frontend/bible-quiz-webapp/src/pages/Question Page/QuestionPage.js
@@ -1,110 +1,140 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import axios from "axios";
 import Loader from "react-loaders";
 import "./QuestionPage.scss";
 
 function QuestionPage(props) {
-    const [questions, setQuestions] = useState([]);
-    const [index, setIndex] = useState(0);
-    const [answers, setAnswers] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    // A function to fetch the questions from the API
-    const fetchQuestions = async () => {
-        try {
-        // Make a GET request to the API endpoint
-            const response = await axios.get(
-                "http://kamvamindpal.com/v1/questions"
-            );
+    // method to fetch the questions from the API
+  const fetchQuestions = async () => {
+    try {
+      // Make a GET request to the API endpoint
+      const response = await axios.get(
+        "http://kamvamindpal.com/v1/questions"
+      );
+      // Set the questions state with the data
+      setQuestions(response.data.data.questions);
+      console.log(response.data.data.questions)
 
-        // Set the questions state with the data
-            setQuestions(response.data);
+      // Set the loading state to false
+        setLoading(false);
+    } catch (error) {
+      // Handle the error
+      console.error(error);
+      alert("Something went wrong. Please try again later.");
+    }
+  };
 
-        // Set the loading state to false
-            setLoading(false);
-        } catch (error) {
-            // Handle the error
-            console.error(error);
-                alert("Something went wrong. Please try again later.");
-        }
-    };
+  const [backgroundImageIndex, setBackgroundImageIndex] = useState(0);
 
-    // A function to handle the user answer selection
+    const backgroundImages = useMemo(() => [
+        'url(https://images.unsplash.com/photo-1600546706018-9e1267462ddc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHdvcmQlMjBvZiUyMEdvZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60)',
+        'url(https://img.freepik.com/premium-photo/happy-epiphany-day-three-kings-day-christian-feast-day-generative-ai_870262-12465.jpg?size=626&ext=jpg&ga=GA1.1.1874357867.1690841151&semt=ais)',
+        'url(https://img.freepik.com/free-photo/christian-cross-nature_23-2150592223.jpg?size=626&ext=jpg&ga=GA1.1.1874357867.1690841151&semt=ais)',
+        'url(https://img.freepik.com/premium-photo/happy-epiphany-day-three-kings-day-christian-feast-day-generative-ai_870262-12465.jpg?size=626&ext=jpg&ga=GA1.1.1874357867.1690841151&semt=ais)',
+    ], []);
+
+    // A function to change background image
+    const changeBackground = useCallback((increment) => {
+        const newIndex = (backgroundImageIndex + increment + backgroundImages.length) % backgroundImages.length;
+        setBackgroundImageIndex(newIndex);
+        console.log(backgroundImageIndex)
+    },[backgroundImageIndex, backgroundImages]);
+    
+    useEffect(() => {
+        console.log("useEffect triggered"); 
+        const interval = setInterval(() => {
+            changeBackground(1);
+        }, 9000); 
+    
+        return () => clearInterval(interval); // Clear the interval on component unmount
+    }, [changeBackground]);
+
     const handleAnswer = (e) => {
-        // Get the value of the selected option
         const value = e.target.value;
         // Update the answers state with the value at the current index
-        setAnswers((prev) => {
-            return [...prev.slice(0, index), value, ...prev.slice(index + 1)];
+            setAnswers((prev) => {
+        return [...prev.slice(0, index), value, ...prev.slice(index + 1)];
         });
     };
 
-        // A function to handle the next button click
-        const handleNext = () => {
+    // Define a function to handle the next button click
+    const handleNext = () => {
+        // Check if the user has answered the current question
+        if (answers[index] == null) {
+            alert("Please select an answer before proceeding");
+        } else {
+            setIndex((prev) => prev + 1);
+        }
+    };
 
-            // Check if the user has answered the current question
-            if (answers[index] == null) {
-                alert("Please select an answer before proceeding");
-            } else {
-                // Increment the index state by one
-                setIndex((prev) => prev + 1);
-            }
-        };
+  // Define a function to handle the submit button click
+    const handleSubmit = () => {
+    // Check if the user has answered all the questions
+        if (answers.length < questions.length) {
+        alert("Please answer all the questions before submitting");
+        } else {
+        // Pass the user answers to the parent component
+            props.onQuizSubmit(answers);
+        }
+    };
 
-        // A function to handle the submit button click
-        const handleSubmit = () => {
-            // Check if the user has answered all the questions
-            if (answers.length < questions.length) {
-                alert("Please answer all the questions before submitting");
-            } else {
-                // Pass the user answers to the parent component
-                props.onQuizSubmit(answers);
-            }
-        };
-
-        useEffect(() => {
-            fetchQuestions();
-        }, []);
+  // Use useEffect hook to fetch the questions when the component mounts
+    useEffect(() => {
+        fetchQuestions();
+    }, []);
 
   return (
-    <div className="question-page">
-        {loading ? (
-            <Loader type='pacman'/>
-        ) : (
-            <div className="question-center">
-                <h1>Quiz</h1>
-                <p>
-                    Question {index + 1} of {questions.length}
-                </p>
-                {questions.length > 0 && (
-                    <div className="question">
-                        <h2>{questions[index].text}</h2>
-                        <div className="options">
-                            {questions[index].answers.map((option, i) => (
-                            <div key={option.id} className="option">
-                                <input
-                                    type="radio"
-                                    id={`option-${i}`}
-                                    name="answer"
-                                    value={option.text}
-                                    checked={answers[index] === option.text}
-                                    onChange={handleAnswer}
-                                />
-                                <label htmlFor={`option-${i}`}>{option.text}</label> 
-                            </div>
-                            ))}
-                        </div>
+    <div 
+    className="landing-page"
+        style={{
+            backgroundImage: `radial-gradient(circle, rgba(15,4,71,0.0984768907563025) 0%, rgba(7,4,48,0.7595413165266106) 58%), ${backgroundImages[backgroundImageIndex]}`,
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover'
+        }}>
+      {loading ? (
+        <div><Loader/> </div>
+      ) : (
+        <div className="question-center">
+            <h1>Quiz</h1>
+            <p>
+                Question {index + 1} of {questions.length}
+            </p>
+            {questions.length > 0 && (
+                <div className="question">
+                <h2>{questions[index].text}</h2>
+                <div className="options">
+                    {questions[index].answers.map((option, i) => (
+                    <div key={option.id} className="option">
+                        <input
+                        type="radio"
+                        id={`option-${i}`}
+                        name="answer"
+                        value={option.text}
+                        checked={answers[index] === option.text}
+                        onChange={handleAnswer}
+                        />
+                        <label htmlFor={`option-${i}`}>{option.text}</label>
                     </div>
-                )}
-                {index === questions.length - 1 ? (
-                    <button onClick={handleSubmit}>Submit</button>
-                ) : (
-                    <button onClick={handleNext}>Next</button>
-                )}
-            </div>
-        )}
+                    ))}
+                </div>
+                </div>
+            )}
+          {index === questions.length - 1 ? (
+            <button onClick={handleSubmit}>Submit</button>
+          ) : (
+            <button onClick={handleNext}>Next</button>
+          )}
+        </div>
+      )}
     </div>
   );
 }
+
 
 export default QuestionPage;
--- frontend/bible-quiz-webapp/src/pages/Result Page/ResultPage.js
+++ frontend/bible-quiz-webapp/src/pages/Result Page/ResultPage.js
@@ -1,23 +1,51 @@
 import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
+import Loader from "react-loaders";
+import { useMemo } from "react";
 
 function ResultPage(props) {
     const [results, setResults] = useState([]);
     const [correct, setCorrect] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    const [backgroundImageIndex, setBackgroundImageIndex] = useState(0);
+
+    const backgroundImages = useMemo(() => [
+        'url(https://images.unsplash.com/photo-1600546706018-9e1267462ddc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHdvcmQlMjBvZiUyMEdvZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60)',
+        'url(https://img.freepik.com/premium-photo/happy-epiphany-day-three-kings-day-christian-feast-day-generative-ai_870262-12465.jpg?size=626&ext=jpg&ga=GA1.1.1874357867.1690841151&semt=ais)',
+        'url(https://img.freepik.com/free-photo/christian-cross-nature_23-2150592223.jpg?size=626&ext=jpg&ga=GA1.1.1874357867.1690841151&semt=ais)',
+        'url(https://img.freepik.com/premium-photo/happy-epiphany-day-three-kings-day-christian-feast-day-generative-ai_870262-12465.jpg?size=626&ext=jpg&ga=GA1.1.1874357867.1690841151&semt=ais)',
+    ], []);
+
+    // A function to change background image
+    const changeBackground = useCallback((increment) => {
+        const newIndex = (backgroundImageIndex + increment + backgroundImages.length) % backgroundImages.length;
+        setBackgroundImageIndex(newIndex);
+        console.log(backgroundImageIndex)
+    },[backgroundImageIndex, backgroundImages]);
+    
+    useEffect(() => {
+        console.log("useEffect triggered"); 
+        const interval = setInterval(() => {
+            changeBackground(1);
+        }, 9000); 
+    
+        return () => clearInterval(interval); // Clear the interval on component unmount
+    }, [changeBackground]);
+
+
     // A function to fetch the results from the API
     const fetchResults = useCallback(async () => {
         try {
-            // Make a POST request to the API endpoint with the user answers
+            // Making a POST request to the API endpoint with the user answers
             const response = await axios.post(
                 "https://example.com/api/results",
                 props.answers
             );
             // Set the results state with the data
-            setResults(response.data);
+            setResults(response.data.data);
             // Set the correct state with the number of correct answers
-            setCorrect(response.data.filter((result) => result.correct).length);
+            setCorrect(response.data.data.filter((result) => result.correct).length);
             // Set the loading state to false
             setLoading(false);
         } catch (error) {
@@ -38,12 +66,18 @@ function ResultPage(props) {
     }, [fetchResults]);
 
   return (
-    <div className="result-page">
+    <div className="result-page"
+        style={{
+            backgroundImage: `radial-gradient(circle, rgba(15,4,71,0.0984768907563025) 0%, rgba(7,4,48,0.   7595413165266106) 58%), ${backgroundImages[backgroundImageIndex]}`,
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover'
+        }}>
         {loading ? (
-            <p>Loading...</p>
+            <Loader type='pacman'/>
         ) : (
-            <>
-                <h1>Quiz App</h1>
+            <div className="result-center">
+                <h1>Quiz Result</h1>
                 <p>
                     You scored {correct} out of {results.length}
                 </p>
@@ -67,7 +101,7 @@ function ResultPage(props) {
             ))}
         </div>
         <button onClick={handleRestart}>Restart</button>
-            </>
+            </div>
         )}
     </div>
   );
